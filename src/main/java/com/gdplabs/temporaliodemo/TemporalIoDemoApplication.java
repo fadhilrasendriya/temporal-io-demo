3e4deba68f9a321b10c915f91ff4ce326f1d53a3
Fadhil Rasendriya Prabowo <fadhil.rasendriya@gmail.com>
2021-07-08 08:39:36 +0700

    refactor init method

--- src/main/java/com/gdplabs/temporaliodemo/TemporalIoDemoApplication.java
+++ src/main/java/com/gdplabs/temporaliodemo/TemporalIoDemoApplication.java
@@ -1,26 +1,13 @@
 package com.gdplabs.temporaliodemo;
 
-import com.gdplabs.temporaliodemo.simpleapproval.temporal.ApprovalActivities;
-import com.gdplabs.temporaliodemo.simpleapproval.temporal.ApprovalWorkflow;
-import com.gdplabs.temporaliodemo.simpleapproval.temporal.ApprovalWorkflowImpl;
-import io.temporal.worker.Worker;
-import io.temporal.worker.WorkerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class TemporalIoDemoApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext appContext = SpringApplication.run(TemporalIoDemoApplication.class, args);
-        WorkerFactory factory= appContext.getBean(WorkerFactory.class);
-        ApprovalActivities activities = appContext.getBean(ApprovalActivities.class);
-        Worker worker = factory.newWorker(ApprovalWorkflow.QUEUE_NAME);
-        worker.registerWorkflowImplementationTypes(ApprovalWorkflowImpl.class);
-        worker.registerActivitiesImplementations(activities);
-        factory.start();
+        SpringApplication.run(TemporalIoDemoApplication.class, args);
     }
 
 }
--- src/main/java/com/gdplabs/temporaliodemo/simpleapproval/config/ApprovalConfig.java
+++ src/main/java/com/gdplabs/temporaliodemo/simpleapproval/config/ApprovalConfig.java
@@ -3,6 +3,7 @@ package com.gdplabs.temporaliodemo.simpleapproval.config;
 import com.gdplabs.temporaliodemo.simpleapproval.temporal.ApprovalActivities;
 import com.gdplabs.temporaliodemo.simpleapproval.temporal.ApprovalActivitiesImpl;
 import com.gdplabs.temporaliodemo.simpleapproval.temporal.ApprovalWorkflow;
+import com.gdplabs.temporaliodemo.simpleapproval.temporal.ApprovalWorkflowImpl;
 import io.temporal.client.WorkflowClient;
 import io.temporal.client.WorkflowClientOptions;
 import io.temporal.serviceclient.WorkflowServiceStubs;
@@ -10,12 +11,15 @@ import io.temporal.serviceclient.WorkflowServiceStubsOptions;
 import io.temporal.worker.Worker;
 import io.temporal.worker.WorkerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+
 
 @Component
 @Configuration
@@ -24,6 +28,8 @@ public class ApprovalConfig {
 
     private String temporalNamespace = "default";
 
+    @Autowired
+    private ApplicationContext context;
 
     @Bean
     public WorkflowServiceStubs workflowServiceStubs() {
@@ -45,6 +51,16 @@ public class ApprovalConfig {
     @Bean
     public ApprovalActivitiesImpl approvalActivities() {
         return new ApprovalActivitiesImpl();
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initWorker() {
+        WorkerFactory factory= context.getBean(WorkerFactory.class);
+        ApprovalActivities activities = context.getBean(ApprovalActivities.class);
+        Worker worker = factory.newWorker(ApprovalWorkflow.QUEUE_NAME);
+        worker.registerWorkflowImplementationTypes(ApprovalWorkflowImpl.class);
+        worker.registerActivitiesImplementations(activities);
+        factory.start();
     }
 
 
